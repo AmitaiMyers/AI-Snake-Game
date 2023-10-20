cac8b8ee38fc6d701eb00085f2367582607b931e
AmitaiMyers <73966096+AmitaiMyers@users.noreply.github.com>
2023-10-20 15:37:34 +0300

    part 3

--- agent.py
+++ agent.py
@@ -4,5 +4,70 @@ import numpy as np
 from snake_game_AI import SnakeGameAI, Direction, Point
 from collections import deque
 
+MAX_MEMORY = 100_000
+BATCH_SIZE = 1000
+LR = 0.001
 
 
+class Agent:
+    def __init__(self):
+        self.n_games = 0
+        self.epsilon = 0  # randomness
+        self.gamma = 0  # discount rate
+        self.memory = deque(maxlen=MAX_MEMORY)  # popleft()
+        # TODO: model, trainer
+
+    def get_state(self, game):
+        pass
+
+    def remember(self, state, action, reward, next_state, done):
+        pass
+
+    def train_long_memory(self):
+        pass
+
+    def train_short_memory(self, state, action, reward, next_state, done):
+        pass
+
+    def get_action(self, state):
+        pass
+
+
+def train():
+    plot_scores = []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
+
+        # get move
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        #remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # train long memory, plot result
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+
+            if score > record:
+                record = score
+                # agent.model.save()
+
+            print(' Game: ', agent.n_games, ' Score: ', score, ' Record: ',record)
+
+if __name__ == '__main__':
+    train()
